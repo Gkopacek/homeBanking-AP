ab9d7861959d28ae526f0fd38e6f0cf7d596ca9e
Guillermo Kopacek <kopacek5@gmail.com>
2023-08-03 21:10:04 -0300

    Se agregan el metodo initData() junto con la primer anotation de @bean dentro del metodo principal

--- src/main/java/com/ap/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/ap/mindhub/homebanking/HomebankingApplication.java
@@ -1,13 +1,22 @@
 package com.ap.mindhub.homebanking;
 
+import com.ap.mindhub.homebanking.models.Client;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
+	}
+@Bean
+public CommandLineRunner initdata(){
+		return (args -> {
+			//Client client1 = new Client(1,"Guillermo","Kopacek","kopacek5@gmail.com");
+		});
 	}
 
 }
