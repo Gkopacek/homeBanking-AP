b740ac6cc8ba34053b41b57e00685138757ffd1d
Guillermo Kopacek <kopacek5@gmail.com>
2023-08-06 20:32:18 -0300

    Se crea la interface AccountRepository que extiende de JpaRepository

---
+++ src/main/java/com/ap/mindhub/homebanking/repositories/AccountRepository.java
@@ -0,0 +1,8 @@
+package com.ap.mindhub.homebanking.repositories;
+
+import com.ap.mindhub.homebanking.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository  extends JpaRepository<Account,Double> {
+
+}
