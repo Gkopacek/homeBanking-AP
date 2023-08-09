2bb2b95c5e2b4e0b68c393eb2fe83a1fa0124bd9
Guillermo Kopacek <kopacek5@gmail.com>
2023-08-09 06:48:36 -0300

    Se comienzan a agregar atributos a la clase transaction y se crea una clase de tipo enum con nombre TransactionType

--- src/main/java/com/ap/mindhub/homebanking/models/Transaction.java
+++ src/main/java/com/ap/mindhub/homebanking/models/Transaction.java
@@ -1,4 +1,15 @@
 package com.ap.mindhub.homebanking.models;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Transaction {
+    private long id;
+    private double amount;
+    private String description;
+    private LocalDateTime date;
+
+
+
+
 }
