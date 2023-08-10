5519bd05a88fbda3224a52e6ae20a524125eee4d
Guillermo Kopacek <kopacek5@gmail.com>
2023-08-09 22:13:23 -0300

    Se crea la clase TransactionDTO con sus atributos metodo constructor de copia y getters

--- src/main/java/com/ap/mindhub/homebanking/controllers/AccountController.java
+++ src/main/java/com/ap/mindhub/homebanking/controllers/AccountController.java
@@ -1,2 +1,10 @@
-package com.ap.mindhub.homebanking.controllers;public class AccountController {
+package com.ap.mindhub.homebanking.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class AccountController {
+
 }
